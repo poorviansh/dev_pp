cfd2cd95a68c51c6b6acfa17c2d371bc7c5661ca
Poorvi <poorvians@gmail.com>
2021-03-06 19:08:27 +0530

    javascript

---
+++ lec1_jsintro/intro.js
@@ -0,0 +1,23 @@
+// let a = 10;
+// // console.log(a);
+// if(10 == "10"){
+//     let a = 20;
+//     console.log(a);
+// }
+// console.log(a);
+
+let values = {
+    name: "Poorvi",
+    skills: "Web dev",
+}
+// console.log(values.name);
+// console.log(values.skills);
+
+let key = "i am a value";
+console.log(values[key]);
+
+let arr = [10, true, {
+    place: "Delhi",
+},
+"hello"]
+console.log(arr[2].place);
